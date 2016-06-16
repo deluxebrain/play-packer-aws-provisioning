f89f13b87508ae0bffee78ebcb3c1c22fe8df72b
deluxebrain <matt@deluxebrain.com>
2016-06-16 11:11:47 +0100

    Updated to UTC

--- sample-app/scripts/provision.ps1
+++ sample-app/scripts/provision.ps1
@@ -1,6 +1,7 @@
 (iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')))>$null 2>&1
 choco install 7zip -y
-choco install seek-dsc -y
+# Deprecated - part of PowerShell now
+# choco install seek-dsc -y
 
 # Disable Windows Updates
 cmd /c reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 1 /f
