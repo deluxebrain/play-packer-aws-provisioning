b6c836a966757cd160f79caf34d4a72155b22d15
deluxebrain <matt@deluxebrain.com>
2016-06-18 10:08:53 +0100

    Working on the vagrant file

--- packer-pipeline/scripts/provision-powershell.ps1
+++ packer-pipeline/scripts/provision-powershell.ps1
@@ -1,13 +1,16 @@
 # install PowerShell 5 and Windows Management Framework
+Write-Host "Installing PowerShell"
 choco install powershell -y
 
 # Install NuGet
+Write-Host "Installing NuGet"
 Get-PackageProvider -Name NuGet -ForceBootstrap
 
 # Trust the Microsoft PowerShell Gallery
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
 # Install DSC modules
+Write-Host "Installing Third-Party DSC modules"
 # https://github.com/PowerShell/xWebAdministration
 Install-Module -Name xWebAdministration
 # https://github.com/PowerShell/xNetworking
