58eb5153b2f63a5943a277eb63c20b1fbdb87cf3
deluxebrain <matt@deluxebrain.com>
2016-06-21 23:06:19 +0100

    Fixing broken packer pipeline

--- packer-pipeline/scripts/provision-powershell.ps1
+++ packer-pipeline/scripts/provision-powershell.ps1
@@ -2,19 +2,3 @@
 Write-Host "Installing PowerShell"
 choco install powershell -y
 
-# Install NuGet
-Write-Host "Installing NuGet"
-Get-PackageProvider -Name NuGet -ForceBootstrap
-
-# Trust the Microsoft PowerShell Gallery
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-
-# Install DSC modules
-Write-Host "Installing Third-Party DSC modules"
-# https://github.com/PowerShell/xWebAdministration
-Install-Module -Name xWebAdministration
-# https://github.com/PowerShell/xNetworking
-Install-Module -Name xNetworking
-# List out all modules to verify install
-Get-DscResource
-
