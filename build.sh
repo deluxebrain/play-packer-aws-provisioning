bab7b75024502af3a50984d90bc4f4b55f380795
deluxebrain <matt@deluxebrain.com>
2016-06-30 14:54:10 +0100

    wip

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 export EnableNuGetPackageRestore=true
-nuget "Install" "FAKE" "-OutputDirectory" "Build/packages" "-ExcludeVersion"
-nuget "Install" "seek-dsc-networking" "-Version" "1.0.1" "-OutputDirectory" "Build/packages"
-nuget "Install" "seek-dsc-webadministration" "-Version" "1.0.1" "-OutputDirectory" "Build/packages"
-nuget "Install" "FSharp.Data" "-OutputDirectory" "Build/packages" "-ExcludeVersion"
-mono "Build/packages/FAKE/tools/Fake.exe" build.fsx "$1"
+nuget "Install" "FAKE" "-OutputDirectory" "./build/packages" "-ExcludeVersion"
+#nuget "Install" "seek-dsc-networking" "-Version" "1.0.1" "-OutputDirectory" "Build/packages"
+#nuget "Install" "seek-dsc-webadministration" "-Version" "1.0.1" "-OutputDirectory" "Build/packages"
+nuget "Install" "FSharp.Data" "-OutputDirectory" "./build/packages" "-ExcludeVersion"
+mono "./build/packages/FAKE/tools/Fake.exe" build.fsx "$1"
